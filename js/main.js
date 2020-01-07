3776952deb66723577423aa7da2e2a9455df38ed
Godlove <37009845+ayeahgodlove@users.noreply.github.com>
2020-01-07 04:35:22 -0800

    update
    More responsiveness

--- js/main.js
+++ js/main.js
@@ -13,3 +13,21 @@ let icon = document.querySelector('.icon');
 icon.addEventListener('click', navBar);
 
 // navBar();
+
+// Remove the class w-50 when the sceen size is less than 786px;
+$(document).ready(function() {
+    const alterClass = function() {
+        let ww = document.body.clientWidth;
+        if(ww <786) {
+            $('.list-group').removeClass('w-50');
+        }
+        else if (ww >= 401) {
+            $('.list-group').addClass('w-50');
+        };
+    };
+    $(window).resize(function(){
+        alterClass();
+    });
+      //Fire it when the page first loads:
+      alterClass();
+});
